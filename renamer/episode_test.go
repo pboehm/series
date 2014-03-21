8058dd4a70e32635368fda8206cc5c15acd14b6b
Philipp Böhm <philipp-boehm@live.de>
2014-03-21 17:58:40 +0100

    Added tests for error handling in episode construction

--- renamer/episode_test.go
+++ renamer/episode_test.go
@@ -17,9 +17,22 @@ func (s *MySuite) TestEpisodeExtractionFromFile(c *C) {
     episode, err := CreateEpisodeFromPath(s.FileWithPath("crmi"))
 
     c.Check(err, IsNil)
+    c.Check(episode, Not(IsNil))
     c.Check(episode, FitsTypeOf, new(Episode))
     c.Check(episode.season, Equals, 1)
     c.Check(episode.episode, Equals, 1)
     c.Check(episode.name, Equals, "Testtest mkv")
     c.Check(episode.series, Equals, "Criminal Minds")
+}
+
+func (s *MySuite) TestEpisodeExtractionFromInvalidFile(c *C) {
+    _, err := CreateEpisodeFromPath(s.FileWithPath("illegal1"))
+
+    c.Check(err, ErrorMatches, "Supplied episode has no series information")
+}
+
+func (s *MySuite) TestEpisodeExtractionFromNotExistingFile(c *C) {
+    _, err := CreateEpisodeFromPath("/should/not/exist")
+
+    c.Check(err, ErrorMatches, "Supplied episode does not exist")
 }
